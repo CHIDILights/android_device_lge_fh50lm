de474be25877523336fdc3c5a89cdf4cae1d6177
CHIDILights <62822977+CHIDILights@users.noreply.github.com>
2022-02-26 19:09:14 +0900

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_fh50lm.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_fh50lm-user \
+    omni_fh50lm-userdebug \
+    omni_fh50lm-eng
